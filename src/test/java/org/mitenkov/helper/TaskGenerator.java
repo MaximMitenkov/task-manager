afe5bcbb53ca4a0c71f6aa4b623e4597e2e3b9fd
Maxim Mitenkov <mitmax.official@gmail.com>
2024-09-03 19:11:55 +0300

    TaskGenerator update

--- src/test/java/org/mitenkov/helper/TaskGenerator.java
+++ src/test/java/org/mitenkov/helper/TaskGenerator.java
@@ -1,6 +1,7 @@
 package org.mitenkov.helper;
 
 import org.mitenkov.BaseTest;
+import org.mitenkov.entity.Bug;
 import org.mitenkov.entity.Feature;
 import org.mitenkov.entity.Task;
 import org.mitenkov.enums.Priority;
@@ -24,6 +25,7 @@ public class TaskGenerator extends BaseTest {
                 .id(1)
                 .deadline(LocalDate.now().plusDays(20))
                 .priority(Priority.LOW)
+                .comments(new ArrayList<>())
                 .build();
 
         Feature feature2 = Feature.builder()
@@ -31,11 +33,47 @@ public class TaskGenerator extends BaseTest {
                 .id(2)
                 .deadline(LocalDate.now().plusDays(20))
                 .priority(Priority.LOW)
+                .comments(new ArrayList<>())
+                .build();
+
+        Feature feature3 = Feature.builder()
+                .title("TestFeature3")
+                .priority(Priority.HIGH)
+                .comments(new ArrayList<>())
+                .deadline(LocalDate.now().plusDays(20))
+                .build();
+
+        Bug bug1 = Bug.builder()
+                .deadline(LocalDate.now())
+                .priority(Priority.MEDIUM)
+                .version("2.1.2")
+                .title("TestBug1")
+                .comments(new ArrayList<>())
+                .build();
+
+        Bug bug2 = Bug.builder()
+                .deadline(LocalDate.now())
+                .priority(Priority.LOW)
+                .version("2.0.0")
+                .title("TestBug2")
+                .comments(new ArrayList<>())
+                .build();
+
+        Bug bug3 = Bug.builder()
+                .deadline(LocalDate.now().plusDays(2))
+                .priority(Priority.MEDIUM)
+                .version("1.5.2")
+                .title("TestBug3")
+                .comments(new ArrayList<>())
                 .build();
 
         List<Task> tasks = new ArrayList<>();
         tasks.add(feature1);
         tasks.add(feature2);
+        tasks.add(feature3);
+        tasks.add(bug1);
+        tasks.add(bug2);
+        tasks.add(bug3);
 
         return tasks;
     }
@@ -43,6 +81,17 @@ public class TaskGenerator extends BaseTest {
     public List<Task> generateAndSave() {
         List<Task> tasks = generate();
         taskRepository.saveAll(tasks);
+        return tasks;
+    }
+
+    public List<Task> generateAndSave(int numberOfTasks) {
+        List<Task> tasks = generate();
+        for (int i = 0; i < numberOfTasks; i++) {
+            if (numberOfTasks > tasks.size()) {
+                tasks.addAll(generate());
+            }
+            taskRepository.saveAndFlush(tasks.get(i));
+        }
         return tasks;
     }
 }
