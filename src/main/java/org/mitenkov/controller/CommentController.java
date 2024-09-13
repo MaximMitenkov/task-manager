340ccebb3631bb9ac14958cd16379229d3c16b9f
Maxim Mitenkov <mitmax.official@gmail.com>
2024-09-13 21:32:08 +0300

    Added @ApiResponses, all schemas in api

--- src/main/java/org/mitenkov/controller/CommentController.java
+++ src/main/java/org/mitenkov/controller/CommentController.java
@@ -1,12 +1,17 @@
 package org.mitenkov.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mitenkov.controller.converter.CommentDtoConverter;
 import org.mitenkov.dto.CommentAddRequest;
 import org.mitenkov.dto.CommentDto;
+import org.mitenkov.dto.ErrorMessageDto;
 import org.mitenkov.service.CommentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +21,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/comments")
 @Tag(name = "Comments")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Everything worked correct",
+                content = {@Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = CommentDto.class)
+                )}),
+        @ApiResponse(responseCode = "400", description = "Invalid data supplied, bad request",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+        @ApiResponse(responseCode = "404", description = "Object not found",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class)))})
 @Slf4j
 @AllArgsConstructor
 public class CommentController {
--- src/main/java/org/mitenkov/controller/TaskController.java
+++ src/main/java/org/mitenkov/controller/TaskController.java
@@ -2,12 +2,14 @@ package org.mitenkov.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mitenkov.controller.converter.TaskDtoConverter;
+import org.mitenkov.dto.ErrorMessageDto;
 import org.mitenkov.dto.TaskAddRequest;
 import org.mitenkov.dto.TaskDto;
 import org.mitenkov.enums.SortType;
@@ -23,13 +25,16 @@ import java.util.List;
 @Tag(name = "Tasks")
 @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Everything worked correct",
-                content = @Content(mediaType = "application/json")),
+                content = {@Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = TaskDto.class)
+                )}),
         @ApiResponse(responseCode = "400", description = "Invalid data supplied, bad request",
-                content = @Content),
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
         @ApiResponse(responseCode = "404", description = "Object not found",
-                content = @Content),
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
         @ApiResponse(responseCode = "500", description = "Internal server error",
-                content = @Content)})
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class)))})
 @Slf4j
 @RequiredArgsConstructor
 public class TaskController {
