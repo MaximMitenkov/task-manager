23711266ea246c8a5cf1ec82cafdb6cc011b5103
Maxim Mitenkov <mitmax.official@gmail.com>
2024-09-13 20:00:37 +0300

    some dto as records

--- src/main/java/org/mitenkov/dto/CommentAddRequest.java
+++ src/main/java/org/mitenkov/dto/CommentAddRequest.java
@@ -1,17 +1,7 @@
 package org.mitenkov.dto;
 
-import lombok.Builder;
-import lombok.Data;
-
 import java.sql.Timestamp;
 
-@Data
-@Builder
-public class CommentAddRequest {
-
-    private String content;
-    private String author;
-    private Timestamp dateTime;
-    private Integer TaskId;
+public record CommentAddRequest(String content, String author, Timestamp dateTime, Integer taskId) {
 
 }
