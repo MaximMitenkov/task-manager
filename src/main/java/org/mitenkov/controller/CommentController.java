ca71f81f3e992f6f80a016fb21ecffb2cf35f4a2
Mitenkov Maxim <142478760+MaximMitenkov@users.noreply.github.com>
2024-09-18 20:09:08 +0300

    Spring-doc (#15)
    * Added OpenAPI documentation
    
    * added status codes
    
    * added status codes
    
    * Added @ApiResponses, all schemas in api
    
    * code clean up
    
    * code clean up
    
    * fix

--- src/main/java/org/mitenkov/controller/CommentController.java
+++ src/main/java/org/mitenkov/controller/CommentController.java
@@ -1,10 +1,17 @@
 package org.mitenkov.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mitenkov.controller.converter.CommentDtoConverter;
 import org.mitenkov.dto.CommentAddRequest;
 import org.mitenkov.dto.CommentDto;
+import org.mitenkov.dto.ErrorMessageDto;
 import org.mitenkov.service.CommentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +20,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
+@Tag(name = "Comments")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ok", useReturnTypeSchema = true),
+        @ApiResponse(responseCode = "400", description = "Invalid data supplied, bad request",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+        @ApiResponse(responseCode = "404", description = "Object not found",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class)))
+})
 @Slf4j
 @AllArgsConstructor
 public class CommentController {
@@ -21,6 +38,7 @@ public class CommentController {
     private final CommentDtoConverter commentDtoConverter;
 
     @GetMapping
+    @Operation(summary = "get comments", description = "Get comments by author nickname.")
     public List<CommentDto> getCommentsByNickname(@RequestParam(value = "nick") String nickname) {
         return commentService.findAllByNickname(nickname).stream()
                 .map(commentDtoConverter::toDto)
@@ -28,6 +46,7 @@ public class CommentController {
     }
 
     @PostMapping
+    @Operation(summary = "add comment")
     @ResponseStatus(HttpStatus.CREATED)
     public void createComment(@RequestBody CommentAddRequest request) {
         commentService.add(request);
--- src/main/java/org/mitenkov/controller/TaskController.java
+++ src/main/java/org/mitenkov/controller/TaskController.java
@@ -1,20 +1,36 @@
 package org.mitenkov.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mitenkov.controller.converter.TaskDtoConverter;
+import org.mitenkov.dto.ErrorMessageDto;
 import org.mitenkov.dto.TaskAddRequest;
 import org.mitenkov.dto.TaskDto;
 import org.mitenkov.enums.SortType;
 import org.mitenkov.enums.TaskType;
 import org.mitenkov.service.TaskService;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@Tag(name = "Tasks")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ok", useReturnTypeSchema = true),
+        @ApiResponse(responseCode = "400", description = "Invalid data supplied, bad request",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+        @ApiResponse(responseCode = "404", description = "Object not found",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+                content = @Content(schema = @Schema(implementation = ErrorMessageDto.class)))
+})
 @Slf4j
 @RequiredArgsConstructor
 public class TaskController {
@@ -23,6 +39,7 @@ public class TaskController {
     private final TaskDtoConverter taskDtoConverter;
 
     @GetMapping
+    @Operation(summary = "get tasks", description = "Getting sorted and filtered tasks by type")
     public List<TaskDto> getTasks(
             @RequestParam(value = "sort", required = false) SortType sort,
             @RequestParam(value = "type", required = false) TaskType type
@@ -34,12 +51,13 @@ public class TaskController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "add task", description = "Add task. For task type BUG version is required")
     public void addTask(@RequestBody TaskAddRequest request) {
         taskService.addTask(request);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "delete task", description = "Delete task with written id")
     public void deleteTaskById(@PathVariable(value = "id") int id) {
         taskService.removeTask(id);
     }
