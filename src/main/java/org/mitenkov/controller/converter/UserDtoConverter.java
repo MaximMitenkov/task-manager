4f5febf670c7598f142c09c5cbc7151312263553
Maxim Mitenkov <mitmax.official@gmail.com>
2024-10-01 23:15:58 +0300

    UserDtoConverter through Builder

--- src/main/java/org/mitenkov/controller/converter/UserDtoConverter.java
+++ src/main/java/org/mitenkov/controller/converter/UserDtoConverter.java
@@ -1,19 +1,17 @@
 package org.mitenkov.controller.converter;
 
-import lombok.RequiredArgsConstructor;
 import org.mitenkov.dto.UserDto;
 import org.mitenkov.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class UserDtoConverter {
     public UserDto toDto(User user) {
-        return new UserDto(
-                user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                user.isActive()
-        );
+        return UserDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .isActive(user.isActive())
+                .build();
     }
 }
--- src/main/java/org/mitenkov/dto/UserDto.java
+++ src/main/java/org/mitenkov/dto/UserDto.java
@@ -1,5 +1,8 @@
 package org.mitenkov.dto;
 
+import lombok.Builder;
+
+@Builder
 public record UserDto(
         int id,
         String username,
